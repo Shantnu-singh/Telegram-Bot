92811a6369a963c2116319e21e72473f3df6a9c6
Shantnu-singh <singhshantnu2001@gmail.com>
2024-11-01 12:54:44 +0530

    Added database functionalities

--- bot.py
+++ bot.py
@@ -3,7 +3,7 @@ from telegram import Update
 from telegram.ext import ApplicationBuilder, CommandHandler, ContextTypes
 from telegram.ext import Updater , CommandHandler , ContextTypes , ApplicationBuilder
 import secretes
-import artical_in_db
+from artical_in_db import Database
 
 Token = secretes.Telegram_Token
  
@@ -17,14 +17,19 @@ async def start(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
 
 async def help(update: Update , context: ContextTypes.DEFAULT_TYPE)-> None:
     await update.message.reply_text(
-            """/hello -> Welcome users
-              /start -> Start the Processes
-              /help  -> This particular Message
-              /articles -> Get two Articles With link and summary"""
+            ''' 
+            /hello -> Welcome users
+            /start -> Start the Processes
+            /help  -> This particular Message
+            /articles -> Get two Articles With link and summary
+            '''
               )
     
-async def help(update: Update , context: ContextTypes.DEFAULT_TYPE)-> None:
-    pass
+async def articles(update: Update , context: ContextTypes.DEFAULT_TYPE)-> None:
+    db_obj = Database()
+    aticals = db_obj.pop(numRow=3 , maxLimit= 344)
+    
+    await update.message.reply_text(f"Artical 1 : {aticals[0][0][2]}\n Artical 2 : {aticals[1][0][2]}\n ") 
    
 app = ApplicationBuilder().token(Token).build()
 
@@ -32,6 +37,7 @@ app = ApplicationBuilder().token(Token).build()
 app.add_handler(CommandHandler("hello", hello))
 app.add_handler(CommandHandler("start", start))
 app.add_handler(CommandHandler("help" , help))
+app.add_handler(CommandHandler("articles" , articles))
 
 # Start the bot
 app.run_polling()
